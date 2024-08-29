5bd11e8c53f05167df37fee9fdbb4256da0634ae
devjava69 <k.trabelsi9@laposte.net>
2024-08-29 15:39:49 +0100

    update list of authorized front end app

--- src/main/java/com/karim/book/config/BeansConfig.java
+++ src/main/java/com/karim/book/config/BeansConfig.java
@@ -1,7 +1,7 @@
 package com.karim.book.config;
 
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -55,7 +55,7 @@ public class BeansConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("https://karimdevwm.github.io"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:8080"));
         config.setAllowedHeaders(Arrays.asList(
             HttpHeaders.ORIGIN,
             HttpHeaders.CONTENT_TYPE,
